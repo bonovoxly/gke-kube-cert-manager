016a5fa8e0d5edb255f394a81e8018e0ce0b5713
Bill Cawthra <bill.cawthra@pokitdok.com>
2018-03-21 05:52:39 -0400

    updated test site. added a configmap volumemount

--- test-site/deploy.sh
+++ test-site/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 kubectl create -f cert.yml 
+kubectl create configmap test-index --from-file=./index.html --from-file=./50x.html
 kubectl create -f test-deployment.yml
 kubectl create -f service.yml
 kubectl create -f ingress.yml
